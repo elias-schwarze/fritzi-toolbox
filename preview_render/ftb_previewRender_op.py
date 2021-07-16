4648f01dddd8544a11497a088987468aa198de5a
elias-schwarze <schwarze.elias@gmail.com>
2021-07-16 11:36:42 +0200

    another potential fix for preview camera not updating position when another camera is already present in the scene

--- preview_render/ftb_previewRender_op.py
+++ preview_render/ftb_previewRender_op.py
@@ -53,13 +53,15 @@ class FTB_OT_Preview_Render_Op(Operator):
         previewCamObject.location = (0, -5.53, 1)
         previewCamObject.rotation_euler = ((90*math.pi/180), 0, 0)
 
-        if (wm.bRenderGrid):
-            bpy.ops.mesh.primitive_grid_add(x_subdivisions=60, y_subdivisions=60, size=6,
-                                            enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(0, 0, 0))
+        # if (wm.bRenderGrid):
+        #     bpy.ops.mesh.primitive_grid_add(x_subdivisions=60, y_subdivisions=60, size=6,
+        #                                     enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(0, 0, 0))
 
         bpy.context.view_layer.update()
+        bpy.context.view_layer.depsgraph.update()
         bpy.context.scene.camera = previewCamObject
         bpy.context.view_layer.update()
+        bpy.context.view_layer.depsgraph.update()
 
         totalRenderCount = sum(renderElements)
         currentRenderCount = 0
@@ -68,78 +70,121 @@ class FTB_OT_Preview_Render_Op(Operator):
         wm.progress_update(0)
 
         if wm.bEnableFront:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = 0
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_front")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnableBack:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = math.pi
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_back")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnableLeft:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = -90*math.pi/180
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_left")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnableRight:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = 90*math.pi/180
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_right")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnable45FrontLeft:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = -45*math.pi/180
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_45frontleft")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnable45FrontRight:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = 45*math.pi/180
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_45frontright")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnable45RearLeft:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = -135*math.pi/180
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_45rearleft")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnable45RearRight:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = 135*math.pi/180
             bpy.context.scene.render.filepath = (
                 wm.sOutputPath + wm.sFileName + "_45rearright")
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnableTop:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = 0
             previewEmpty.rotation_euler[0] = -90*math.pi/180
             previewCamObject.location[2] = 0
@@ -148,9 +193,14 @@ class FTB_OT_Preview_Render_Op(Operator):
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         if wm.bEnableBottom:
+            bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
+
             previewEmpty.rotation_euler[2] = 0
             previewEmpty.rotation_euler[0] = 90*math.pi/180
             previewCamObject.location[2] = 0
@@ -159,7 +209,9 @@ class FTB_OT_Preview_Render_Op(Operator):
             bpy.ops.render.render(write_still=1)
             currentRenderCount += 1
             wm.progress_update(currentRenderCount)
+
             bpy.context.view_layer.update()
+            bpy.context.view_layer.depsgraph.update()
 
         bpy.context.scene.render.filepath = storedRenderPath
         bpy.context.scene.camera = storedRenderCam
