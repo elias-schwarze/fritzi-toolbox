1147fdd8fa60956b0ec660f4c3ba46d626607805
elias-schwarze <schwarze.elias@gmail.com>
2021-09-20 16:06:54 +0200

    v0.1.5

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "author": "Elias Schwarze",
     "description": "A suite of tools for the Fritzi Project",
     "blender": (2, 93, 0),
-    "version": (0, 1, 4),
+    "version": (0, 1, 5),
     "location": "3D Viewport > Properties panel (N) > FTB Tab",
     "warning": "Deactivate old version, then restart Blender before installing a newer version",
     "category": "Object"
