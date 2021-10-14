2d68f48fabcfa8a6c05f2d75cd44bf10462e2d2b
elias-schwarze <schwarze.elias@gmail.com>
2021-10-14 11:57:32 +0200

    add function to add preview line art to scene

--- default_setup/ftb_default_lineart_op.py
+++ default_setup/ftb_default_lineart_op.py
@@ -32,9 +32,11 @@ class FTB_OT_DefaultAddLineart_Op(Operator):
         # bpy.ops.object.gpencil_add(align='WORLD', location=(
         #     0, 0, 0), scale=(1, 1, 1), type='LRT_COLLECTION')
         previewLineData = bpy.data.grease_pencils.new(name='ftb_previewLines')
-        previewLineData.layers.new("gplayer", set_active=True)
+        previewLineLayer = previewLineData.layers.new(
+            "gplayer", set_active=True)
+        previewLineLayer.frames.new(frame_number=1)
 
-        previewLineData.stroke_depth_order = '3D'
+        previewLineData.stroke_depth_order = '2D'
         previewLineData.pixel_factor = 0.1
 
         previewLineData.stroke_thickness_space = 'WORLDSPACE'
@@ -49,6 +51,18 @@ class FTB_OT_DefaultAddLineart_Op(Operator):
 
         # assign Material to object
         previewLineObj.data.materials.append(previewLineMaterial)
+
+        previewLineObj.show_in_front = True
+
+        # add line art modifier to gp object
+        lineModifier = previewLineObj.grease_pencil_modifiers.new(
+            "FS_Lines", 'GP_LINEART')
+
+        lineModifier.target_layer = "gplayer"
+        lineModifier.target_material = previewLineMaterial
+        lineModifier.source_collection = bpy.context.view_layer.active_layer_collection.collection
+
+        # lineModifier.source_collection
         bpy.context.scene.collection.objects.link(previewLineObj)
 
         return {'FINISHED'}
