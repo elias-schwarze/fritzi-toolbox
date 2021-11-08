4ffdd8afc91ad958842a684160dbbd9d209bfe2c
elias-schwarze <schwarze.elias@gmail.com>
2021-11-08 16:43:57 +0100

    add output path property and expose to ui for bvh-batch-exporter

--- fbxToBvh_processor/ftb_fbxToBvh_op.py
+++ fbxToBvh_processor/ftb_fbxToBvh_op.py
@@ -2,7 +2,7 @@ import bpy
 from bpy.types import Operator
 from bpy_extras.io_utils import ImportHelper
 from bpy.types import OperatorFileListElement
-from bpy.props import CollectionProperty, StringProperty
+from bpy.props import BoolProperty, CollectionProperty, StringProperty
 
 
 class FTB_OT_BatchFbxBvh_Op(Operator, ImportHelper):
@@ -18,13 +18,27 @@ class FTB_OT_BatchFbxBvh_Op(Operator, ImportHelper):
     # String Property accessed by ImportHelper to store directory path of selected files
     directory: StringProperty(subtype='DIR_PATH')
 
+    sOutputPath: StringProperty(
+        subtype='DIR_PATH', name="Output path")
+
+    use_setting: BoolProperty(
+        name="Example Boolean",
+        description="Example Tooltip",
+        default=True,
+    )
+
     # should only work in object mode
     @classmethod
     def poll(cls, context):
         obj = context.object
+        wm = bpy.context.window_manager
+
+        if (wm.bvhOutputPath == ""):
+            return False
 
         if not obj:
             return True
+
         if obj:
             if obj.mode == "OBJECT":
                 return True
@@ -32,10 +46,11 @@ class FTB_OT_BatchFbxBvh_Op(Operator, ImportHelper):
 
     def execute(self, context):
 
-        objectList = list()
+        wm = bpy.context.window_manager
 
-        for obj in bpy.context.collection.all_objects:
-            objectList.append(obj)
+        if (not wm.bvhOutputPath):
+            self.report({'WARNING'}, 'Output path is not set')
+            return {'CANCELLED'}
 
         if (not self.files):
             self.report({'WARNING'}, "No files selected")
@@ -46,6 +61,12 @@ class FTB_OT_BatchFbxBvh_Op(Operator, ImportHelper):
                 if file.name == "":
                     self.report({'WARNING'}, "No files selected")
                     return{'CANCELLED'}
+
+        objectList = list()
+
+        for obj in bpy.context.collection.all_objects:
+            objectList.append(obj)
+
         # list to keep track of already imported objects between each single import, so that we can find out the newest imported object and modify it
         tempNameList = list()
 
@@ -53,6 +74,8 @@ class FTB_OT_BatchFbxBvh_Op(Operator, ImportHelper):
 
             bpy.ops.import_scene.fbx(filepath=(self.directory + file.name), global_scale=100,
                                      use_image_search=False, automatic_bone_orientation=True)
+            tempNameList.append(file.name)
+        print(tempNameList)
 
         bpy.ops.object.select_all(action='DESELECT')
 
--- fbxToBvh_processor/ftb_fbxToBvh_pnl.py
+++ fbxToBvh_processor/ftb_fbxToBvh_pnl.py
@@ -1,5 +1,6 @@
 import bpy
 from bpy.types import Panel
+from bpy.props import StringProperty
 
 
 class FTB_PT_FbxToBvh_Panel(Panel):
@@ -9,8 +10,15 @@ class FTB_PT_FbxToBvh_Panel(Panel):
     bl_category = "FTB Batching"
     bl_options = {"DEFAULT_CLOSED"}
 
+    bpy.types.WindowManager.bvhOutputPath = StringProperty(
+        subtype='DIR_PATH', name="Output path")
+
     def draw(self, context):
         layout = self.layout
+
+        col = layout.column()
+        col.label(text="Output Directory:")
+        col.prop(context.window_manager, "bvhOutputPath", text="")
 
         col = layout.column()
         col.operator("object.batch_fbx_bvh")
