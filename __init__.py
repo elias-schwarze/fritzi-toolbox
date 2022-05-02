4323e5660e8dcc588c50ced970b6dfca93f8451d
elias-schwarze <schwarze.elias@gmail.com>
2022-05-02 11:11:03 +0200

    v 0.6.0

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ bl_info = {
     "author": "Elias Schwarze, Robert Lehmann",
     "description": "A suite of tools for the Fritzi Project",
     "blender": (3, 0, 0),
-    "version": (0, 5, 1),
+    "version": (0, 6, 0),
     "location": "3D Viewport > Properties panel (N) > FTB Tab",
     "category": "Object"
 }
