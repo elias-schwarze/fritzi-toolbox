0665c742ed739c75e3252147b91416587ae39270
elias-schwarze <schwarze.elias@gmail.com>
2022-07-21 10:23:29 +0200

    small refactor for geo node setup (only one function, shorter and readable)

--- data_editing/ftb_gnodesMaterialReplacer_op.py
+++ data_editing/ftb_gnodesMaterialReplacer_op.py
@@ -1,46 +1,51 @@
 import bpy
 
-def createSetMatNodes():
-        setMatNodes = bpy.data.node_groups.new(name="FTB_setLocalMaterial", type='GeometryNodeTree')
+def createSetMatNodes(replaceMat = False):
+    """
+    Create a Geometry Node Setup to replace Materials on any Mesh 
+    param replaceMat: Set to True to create a setup to replace only specific Material slots, set to False to create setup that overrides all material slots of the given object
+    """
 
-        setMatNodes.inputs.new(name="Geometry", type="NodeSocketGeometry")
-        setMatNodes.inputs.new(name="Material", type="NodeSocketMaterial")
-        setMatNodes.outputs.new(name="Geometry", type="NodeSocketGeometry")
+    # create node group and set name
+    if replaceMat:
+        nodeGroupName = "FTB_replaceLocalMaterial"
 
-        geoInput = setMatNodes.nodes.new(type="NodeGroupInput")
+    else:
+        nodeGroupName = "FTB_setLocalMaterial"
 
-        geoOutput = setMatNodes.nodes.new(type="NodeGroupOutput")
-        geoOutput.location = (700.0, 0.0)
+    setMatNodes = bpy.data.node_groups.new(name=nodeGroupName, type='GeometryNodeTree')
 
-        geoSetMat = setMatNodes.nodes.new(type="GeometryNodeSetMaterial")
-        geoSetMat.location = (350.0, 0.0)
+    # I/O Nodes
+    geoInput = setMatNodes.nodes.new(type="NodeGroupInput")
+    geoOutput = setMatNodes.nodes.new(type="NodeGroupOutput")
+    geoOutput.location = (700.0, 0.0)
 
-        setMatNodes.links.new(input=geoInput.outputs[0], output=geoSetMat.inputs["Geometry"])
-        setMatNodes.links.new(input=geoInput.outputs[1], output=geoSetMat.inputs[2])
-        setMatNodes.links.new(input=geoSetMat.outputs[0], output=geoOutput.inputs[0])
+    # I/O Sockets for the entire node group
+    setMatNodes.inputs.new(name="Geometry", type="NodeSocketGeometry")
 
-def createReplaceMatNodes():
-        setMatNodes = bpy.data.node_groups.new(name="FTB_replaceLocalMaterial", type='GeometryNodeTree')
-
-        setMatNodes.inputs.new(name="Geometry", type="NodeSocketGeometry")
+    if replaceMat:
         setMatNodes.inputs.new(name="Replace", type="NodeSocketMaterial")
         setMatNodes.inputs.new(name="With", type="NodeSocketMaterial")
-        setMatNodes.outputs.new(name="Geometry", type="NodeSocketGeometry")
-
-        geoInput = setMatNodes.nodes.new(type="NodeGroupInput")
-
-        geoOutput = setMatNodes.nodes.new(type="NodeGroupOutput")
-        geoOutput.location = (700.0, 0.0)
-
         geoSetMat = setMatNodes.nodes.new(type="GeometryNodeReplaceMaterial")
-        geoSetMat.location = (350.0, 0.0)
 
-        setMatNodes.links.new(input=geoInput.outputs[0], output=geoSetMat.inputs["Geometry"])
+    else: 
+        setMatNodes.inputs.new(name="Material", type="NodeSocketMaterial")
+        geoSetMat = setMatNodes.nodes.new(type="GeometryNodeSetMaterial")
+
+    setMatNodes.outputs.new(name="Geometry", type="NodeSocketGeometry")
+    geoSetMat.location = (350.0, 0.0)
+
+    # create links between available nodes
+    setMatNodes.links.new(input=geoInput.outputs[0], output=geoSetMat.inputs["Geometry"])
+
+    if replaceMat:
         setMatNodes.links.new(input=geoInput.outputs[1], output=geoSetMat.inputs[1])
         setMatNodes.links.new(input=geoInput.outputs[2], output=geoSetMat.inputs[2])
 
-        setMatNodes.links.new(input=geoSetMat.outputs[0], output=geoOutput.inputs[0])
+    else:
+        setMatNodes.links.new(input=geoInput.outputs[1], output=geoSetMat.inputs[2])
     
+    setMatNodes.links.new(input=geoSetMat.outputs[0], output=geoOutput.inputs[0])
 
 
 class FTB_OT_GnodesSetMaterials_Op(bpy.types.Operator):
@@ -65,7 +70,7 @@ class FTB_OT_GnodesSetMaterials_Op(bpy.types.Operator):
 
     def execute(self, context):
 
-        createReplaceMatNodes()
+        createSetMatNodes(replaceMat=True)
         
         return {'FINISHED'}
 
