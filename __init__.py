5b6bd4cc87a09d69e55892e2c08ae81a689ba076
SFB-A-02\FilmBIlder <MichaelDSchieber@gmx.de>
2022-11-17 15:15:45 +0100

    v0.9.4

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "author": "Elias Schwarze, Robert Lehmann",
     "description": "A suite of tools for the Fritzi Project",
     "blender": (3, 0, 0),
-    "version": (0, 9, 3),
+    "version": (0, 9, 4),
     "location": "3D Viewport > Properties panel (N) > FTB Tab",
     "category": "Object"
 }
