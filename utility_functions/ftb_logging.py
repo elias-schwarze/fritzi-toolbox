ac411e1799712a3f2cb0a286b33dc9c0303dd18c
Robert Lehmann <robert.lehmann@protonmail.com>
2022-12-04 16:40:02 +0100

    Adds a simple logging module providing utility functions for  logging to
    console and handle operator reports at the same time

---
+++ utility_functions/ftb_logging.py
@@ -0,0 +1,51 @@
+import bpy
+import time
+
+
+from enum import Enum, unique
+
+ENDC = "\033[0m"  # ends text formatting and resets to default
+
+
+@unique
+class Severity(Enum):
+    NONE = ""                   # no severity code printed + no color
+    INFO = "\033[94m"           # blue
+    WARNING = "\033[93m"        # yellow
+    ERROR = "\033[91m"          # red
+
+
+def console(self, severity: Severity, message: str) -> None:
+    """
+    Prints the specified message with specific color-coded severity to the console using current time
+    and bl_idname as identifier.
+    :param severity: The severity of the message. (NONE, INFO, WARNING, ERROR)
+    :type severity:'Severity'
+    :param message: The message being printed to the console
+    :type message:'str'
+    """
+    _time_str = time.strftime("%H:%M:%S", time.localtime())
+    _severity_str = (f"[{severity.name}]", "")[severity == Severity.NONE]
+    print(f"[{_time_str}]{severity.value}{_severity_str}{ENDC} {self.bl_idname}: {message}")
+
+
+def report(self, severity: Severity, message: str) -> None:
+    """
+    This should only be called from inside an operator class. Prints a message with specific color-coded
+    severity to the console and also performs a report in blender using the message and severity specified.
+    :param severity: The severity of the message. (NONE, INFO, WARNING, ERROR)
+    :type severity:'Severity'
+    :param message: The message being printed to the console and in the report
+    :type message:'str'
+    """
+    console(self, severity, message)
+    if not issubclass(self.__class__, bpy.types.Operator):
+        _class_name = __name__.split('.')[-1]
+        _error_msg = (f"The {_class_name}.report() method should only be called from inside an operator "
+                      f"class. The current class calling is of type: \"{self.__class__.__name__}\". "
+                      f"Use the {_class_name}.console() method instead")
+        console(self, Severity.ERROR, _error_msg)
+        return
+
+    if severity != Severity.NONE:
+        self.report({severity.name}, message)
