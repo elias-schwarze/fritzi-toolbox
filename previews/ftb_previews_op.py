6c412edf22e8d5f6334199bfe086f3c29d81bd5b
Robert Lehmann <Robert.Lehmann@protonmail.com>
2023-08-10 09:27:26 +0200

    adds feature to export viewport rendered previews based on a framelist

--- previews/ftb_previews_op.py
+++ previews/ftb_previews_op.py
@@ -1,14 +1,14 @@
 import bpy
+import inspect
 import math
+import os
 import re
-
-from bpy.types import Operator
+import sys
 
 from bpy_extras.io_utils import ImportHelper
-from bpy.types import Operator
-from bpy.types import OperatorFileListElement
-from bpy.props import BoolProperty, CollectionProperty
-from bpy.props import StringProperty
+from bpy.types import Operator, OperatorFileListElement
+from bpy.props import BoolProperty, CollectionProperty, StringProperty
+from .. utility_functions import ftb_logging as log
 
 from math import radians
 
@@ -414,15 +414,165 @@ class FTB_OT_Render_Shot_Preview_Op(Operator):
         return {'FINISHED'}
 
 
-classes = (FTB_OT_Preview_Render_Op, FTB_OT_Set_JPG_Output_Op, FTB_OT_Preview_Import_Op, FTB_OT_Preview_Reload_Op,
-           FTB_OT_Render_Shot_Preview_Op)
+class FTB_OT_RenderShotPreviews(Operator):
+    bl_idname = "render.ftb_shot_previews"
+    bl_label = "FTB: Export Shot Previews"
+    bl_description = "Exports viewport stills in rendered view to the render output path of all frames you specified"
+    bl_options = {"REGISTER"}
+
+    input_error_msg: str = ""
+    alternative_seps = {";", ".", "|"}
+
+    def validate_input(self, context):
+        if self.render_frames.isspace() or not self.render_frames:
+            self.invalid_input = True
+            FTB_OT_RenderShotPreviews.input_error_msg = "Input is empty!"
+            return None
+
+        input_has_numbers = False
+        input: str = self.render_frames.replace(" ", "")
+        for char in input:
+            input_has_numbers |= char.isnumeric()
+            valid_char = char == "," or char in FTB_OT_RenderShotPreviews.alternative_seps or char.isnumeric()
+
+            if not valid_char:
+                self.invalid_input = True
+                FTB_OT_RenderShotPreviews.input_error_msg = (
+                    f"Only numbers [0-9] and characters "
+                    f", {''.join((c + ' ') for c in FTB_OT_RenderShotPreviews.alternative_seps)}allowed")
+                return None
+
+        if not input_has_numbers:
+            self.invalid_input = True
+            FTB_OT_RenderShotPreviews.input_error_msg = (f"Enter at least one number")
+            return None
+
+        self.invalid_input = False
+        return None
+
+    invalid_input: BoolProperty(default=False,
+                                options={'HIDDEN'})
+
+    open_render_dir: BoolProperty(name="Open Output directory in Explorer?",
+                                  default=False)
+
+    render_frames: StringProperty(name="Frame List",
+                                  description="Specify a list of frames to export. Use , ; . | as separators",
+                                  options={'TEXTEDIT_UPDATE'},
+                                  update=validate_input)
+
+    @classmethod
+    def poll(cls, context):
+        if context.area.type != 'VIEW_3D':
+            return False
+        if not bpy.data.is_saved:
+            cls.poll_message_set("File is not saved. Save file on disk to use this feature")
+            return False
+        if context.scene.render.image_settings.file_format in {'FFMPEG', 'AVI_RAW', 'AVI_JPEG'}:
+            cls.poll_message_set(
+                "Output file format must be an image-format. Pick an image-format in Output Properties Tab")
+            return False
+        if not context.scene.render.filepath or context.scene.render.filepath.isspace():
+            cls.poll_message_set("Output path is invalid. Specify a valid render output path in Output Properties Tab")
+            return False
+        return True
+
+    def draw(self, context):
+        if self.invalid_input:
+            self.layout.alert = True
+            self.layout.label(text=self.input_error_msg)
+        self.layout.alert = False
+        self.layout.prop(self, "render_frames")
+        row = self.layout.row()
+        row.alignment = 'RIGHT'
+        row.prop(self, "open_render_dir")
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self, width=400)
+
+    def execute(self, context):
+        self.validate_input(context)
+        if self.invalid_input:
+            return context.window_manager.invoke_props_dialog(self, width=400)
+
+        split_sep = ","
+        input: str = self.render_frames
+
+        # clean user input
+        input = input.replace(" ", "")
+        for sep in self.alternative_seps:
+            input = input.replace(sep, split_sep)
+        frames_str = input.split(split_sep)
+        frames_str = set(frames_str)
+        if "" in frames_str:
+            frames_str.remove("")
+
+        frames_int = [int(frame) for frame in frames_str]
+
+        if len(frames_int) < 1:
+            log.report(self, log.Severity.ERROR, f"Could not extract valid frames from input")
+            return {'CANCELLED'}
+
+        frames_int.sort(reverse=True)
+
+        initially_in_camview = context.area.spaces[0].region_3d.view_perspective == 'CAMERA'
+        initial_overlay_setting = context.space_data.overlay.show_overlays
+        initial_shading_type = context.space_data.shading.type
+        initial_frame = context.scene.frame_current
+        initial_path = context.scene.render.filepath
+
+        render_dir = initial_path[0:initial_path.rindex(os.sep)]
+        filename = bpy.data.filepath[bpy.data.filepath.rindex(os.sep):-6]
+
+        def number_to_str_with_leading_char(number: int, leading_char: str, leading_count: int) -> str:
+            return f"{''.join(leading_char for i in range(0, max(0, leading_count - len(str(number)))))}{number}"
+
+        def _initialze_settings():
+            context.scene.frame_current = initial_frame
+            context.scene.render.filepath = initial_path
+            context.space_data.overlay.show_overlays = initial_overlay_setting
+            context.space_data.shading.type = initial_shading_type
+            if not initially_in_camview:
+                bpy.ops.view3d.view_camera()
+
+        if not initially_in_camview:
+            bpy.ops.view3d.view_camera()
+        context.space_data.overlay.show_overlays = False
+        context.space_data.shading.type = 'RENDERED'
+        for frame in frames_int:
+            if frame not in range(context.scene.frame_start, context.scene.frame_end + 1):
+                _initialze_settings()
+                log.report(self, log.Severity.ERROR, f"Frame {frame} is not in sequence range!")
+                return {'CANCELLED'}
+
+            context.scene.frame_set(frame)
+            new_output_path = f"{render_dir}{os.sep}{filename}_frame{number_to_str_with_leading_char(frame, '0', 5)}"
+            context.scene.render.filepath = new_output_path
+            bpy.ops.render.opengl(write_still=True)
+
+        _initialze_settings()
+
+        msg = f"Frames {''.join(str(frame) + ', ' for frame in frames_int)[:-2]} exported to {render_dir}"
+        log.report(self, log.Severity.INFO, msg)
+
+        if self.open_render_dir:
+            os.startfile(render_dir)
+
+        return {'FINISHED'}
+
+
+classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
 
 
 def register():
     for c in classes:
-        bpy.utils.register_class(c)
+        if "FTB_" not in c[0]:
+            continue
+        bpy.utils.register_class(globals()[c[0]])
 
 
 def unregister():
     for c in reversed(classes):
-        bpy.utils.unregister_class(c)
+        if "FTB_" not in c[0]:
+            continue
+        bpy.utils.unregister_class(globals()[c[0]])
--- previews/ftb_previews_pnl.py
+++ previews/ftb_previews_pnl.py
@@ -104,18 +104,20 @@ class FTB_PT_PreviewSelector_Panel(Panel):
                  text="Bottom", toggle=True)
 
 
-def draw_ftb_render_anim_operator(self, context):
+def add_to_3dview_menu(self, context):
     layout = self.layout
-    layout.operator("render.ftb_preview")
+    layout.separator()
+    layout.operator("render.ftb_shot_previews")
+    # layout.operator("render.ftb_preview")
 
 
 def register():
     bpy.utils.register_class(FTB_PT_Previews_Panel)
     bpy.utils.register_class(FTB_PT_PreviewSelector_Panel)
-    # bpy.types.VIEW3D_MT_view.append(draw_ftb_render_anim_operator)
+    bpy.types.VIEW3D_MT_view.append(add_to_3dview_menu)
 
 
 def unregister():
-    # bpy.types.VIEW3D_MT_view.remove(draw_ftb_render_anim_operator)
+    bpy.types.VIEW3D_MT_view.remove(add_to_3dview_menu)
     bpy.utils.unregister_class(FTB_PT_Previews_Panel)
     bpy.utils.unregister_class(FTB_PT_PreviewSelector_Panel)
